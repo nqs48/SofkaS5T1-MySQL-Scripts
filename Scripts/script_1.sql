d904dcbc41cb594217c5d1f2d5adadd65c0fd0d4
Nestor Quiroga Suárez <nqs48@icloud.com>
2022-05-28 00:19:09 -0500

    Data to <customer, supplier>

--- Scripts/script_1.sql
+++ Scripts/script_1.sql
@@ -5,9 +5,9 @@ CREATE SCHEMA IF NOT EXISTS store_db DEFAULT CHARACTER SET utf8;
 USE store_db ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Custumer`
+-- Table custumer
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS custumer (
+CREATE TABLE IF NOT EXISTS customer (
   cus_id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(45) NOT NULL,
   type_doc VARCHAR(3) NOT NULL,
@@ -17,18 +17,18 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table store_db.supplier
+-- Table supplier
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS supplier (
   sup_id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(45) NOT NULL,
-  telephone VARCHAR(12) NOT NULL,
+  telephone VARCHAR(15) NOT NULL,
   PRIMARY KEY (sup_id))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table store_db.product
+-- Table product
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS product (
   prod_id INT NOT NULL AUTO_INCREMENT,
@@ -46,31 +46,31 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table store_db.invoice
+-- Table invoice
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS invoice (
   inv_id INT NOT NULL AUTO_INCREMENT,
   seller VARCHAR(45) NOT NULL,
   product_prod_id INT NOT NULL,
-  Custumer_cus_id INT NOT NULL,
+  customer_cus_id INT NOT NULL,
   PRIMARY KEY (inv_id),
   INDEX fk_invoice_product1_idx (product_prod_id ASC) VISIBLE,
-  INDEX fk_invoice_Custumer1_idx (Custumer_cus_id ASC) VISIBLE,
+  INDEX fk_invoice_customer1_idx (customer_cus_id ASC) VISIBLE,
   CONSTRAINT fk_invoice_product1
     FOREIGN KEY (product_prod_id)
     REFERENCES product (prod_id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT fk_invoice_Custumer1
-    FOREIGN KEY (Custumer_cus_id)
-    REFERENCES custumer (cus_id)
+  CONSTRAINT fk_invoice_customer1
+    FOREIGN KEY (customer_cus_id)
+    REFERENCES customer (cus_id)
     ON DELETE RESTRICT
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table store_db.sale
+-- Table sale
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS sale (
   sale_id INT NOT NULL AUTO_INCREMENT,
@@ -85,3 +85,34 @@ CREATE TABLE IF NOT EXISTS sale (
     ON DELETE RESTRICT
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+
+
+-- -----------------------------------------------------
+-- Insert data customer 
+-- -----------------------------------------------------
+
+INSERT INTO customer (name,type_doc,document)
+VALUES
+  ("Nash Nicholson","cc",211295824),
+  ("Orson Burks","ti",252409685),
+  ("Flynn Carpenter","cc",183427410),
+  ("Karina Mullins","cc",290995022),
+  ("Rosalyn Cannon","cc",598788284);
+
+
+-- -----------------------------------------------------
+--  Insert data supplier
+-- -----------------------------------------------------
+
+INSERT INTO supplier (name,telephone)
+VALUES
+  ("Eu Ltd","1-885-424-7207"),
+  ("Accumsan Interdum Libero Limited","285-5268"),
+  ("Rhoncus Id Corp.","1-447-963-1754"),
+  ("Fringilla Foundation","1-913-245-2863"),
+  ("At Lacus Incorporated","815-1246");
+
+-- -----------------------------------------------------
+--  Insert data product
+-- -----------------------------------------------------
