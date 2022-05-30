f05f38e76bf3bcb769dacac4b151408e864f2181
Nestor Quiroga Suárez <nqs48@icloud.com>
2022-05-29 21:38:28 -0500

    Scripts Completegit status

---
+++ Scripts/script_2.sql
@@ -0,0 +1,13 @@
+-- -----------------------------------------------------------------------------------
+-- 1. SQL query where you can obtain the products sold by entering document type and document number.
+-- -----------------------------------------------------------------------------------
+
+SELECT sale_id,cus_document,product_prod_id FROM invoice INNER JOIN sale ON inv_id=invoice_inv_id  WHERE cus_document = 1004620934 AND type_doc = "TI" AND status = "A";
+SELECT sale_id,cus_document,product_prod_id FROM invoice INNER JOIN sale ON inv_id=invoice_inv_id  WHERE cus_document = 1004960454 AND type_doc = "CC" AND status = "A";
+
+-- -----------------------------------------------------------------------------------
+-- 2. Consult products by name, which must show who or who have been their suppliers.
+-- -----------------------------------------------------------------------------------
+
+SELECT name, supplier_sup_id FROM product WHERE name = 'Monster' ;
+SELECT name, supplier_sup_id FROM product WHERE name = 'apple' ;
